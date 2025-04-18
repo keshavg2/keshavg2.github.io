44ddf5c03c27c84e7e9c13fe8829a65725cb7801
Keshav Gupta <keshavgupta@Mac.lan>
2025-04-18 18:05:15 +0530

    change the data

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,40 +1,32 @@
 import React from "react";
 
 const Portfolio = ({ data }) => {
-  if (data) {
-    var projects = data.projects.map(function (projects) {
-      return (
-        <div key={projects.title} className="columns portfolio-item">
-          <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-              <img alt={projects.title} src={projects.image} />
-              <div className="overlay">
-                <div className="portfolio-item-meta">
-                  <h5>{projects.title}</h5>
-                  <p>{projects.category}</p>
-                </div>
-              </div>
-              <div className="link-icon">
-                <i className="fa fa-link"></i>
-              </div>
-            </a>
+  const projects = data?.projects?.map((project) => (
+    <div key={project.title} className="columns portfolio-item">
+      <div className="item-wrap">
+        <a href={project.url} title={project.title} target="_blank" rel="noopener noreferrer">
+          <img alt={project.title} src={project.image} />
+          <div className="overlay">
+            <div className="portfolio-item-meta">
+              <h5>{project.title}</h5>
+              <p>{project.category}</p>
+            </div>
           </div>
-          <a href={projects.code}>Source code</a>
-        </div>
-      );
-    });
-  }
+          <div className="link-icon">
+            <i className="fa fa-link"></i>
+          </div>
+        </a>
+      </div>
+      <a href={project.code} target="_blank" rel="noopener noreferrer">Source code</a>
+    </div>
+  ));
 
   return (
     <section id="portfolio">
       <div className="row">
         <div className="twelve columns collapsed">
-          <h1>Check Out Some of My projects.</h1>
-
-          <div
-            id="portfolio-wrapper"
-            className="bgrid-quarters s-bgrid-thirds cf"
-          >
+          <h1>Check Out Some of My Projects</h1>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
             {projects}
           </div>
         </div>
